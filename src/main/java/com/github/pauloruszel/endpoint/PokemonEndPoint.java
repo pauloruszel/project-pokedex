d17595d988466668d26ba1b018fb60cdff780ff8
Paulo Ruszel da Luz Santos <paulo.ruszel.santos@gmail.com>
2020-10-27 23:13:44 -0300

    CRUD da entidade Pokemon funcionando.

--- src/main/java/com/github/pauloruszel/endpoint/PokemonEndPoint.java
+++ src/main/java/com/github/pauloruszel/endpoint/PokemonEndPoint.java
@@ -6,6 +6,7 @@ import com.github.pauloruszel.model.Pokemon;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 import javax.transaction.Transactional;
+import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -21,11 +22,45 @@ public class PokemonEndPoint {
         return Pokemon.listAll();
     }
 
+    @GET
+    @Path("{id}")
+    public Response getPokemon(@PathParam("id") Long id) {
+        Pokemon pokemonEncontrado = Pokemon.findById(id);
+        if (pokemonEncontrado != null) {
+            return Response.ok(pokemonEncontrado).status(Response.Status.OK).build();
+        }
+        return Response.ok(pokemonEncontrado).status(Response.Status.NOT_FOUND).build();
+    }
+
     @POST
     @Transactional
-    public Response addPokemon(@RequestBody PokemonDTO dto) {
+    public Response addPokemon(@RequestBody @Valid PokemonDTO dto) {
         Pokemon pokemon = PokemonConverter.converter(dto);
         pokemon.persist();
         return Response.ok(pokemon).status(Response.Status.CREATED).build();
+    }
+
+    @PUT
+    @Transactional
+    public Response updatePokemon(@RequestBody @Valid PokemonDTO dto) {
+        Pokemon pokemon = Pokemon.findById(dto.getId());
+        if (pokemon != null) {
+            Pokemon retorno = PokemonConverter.converter(dto);
+            retorno.getEntityManager().merge(retorno);
+            return Response.ok(retorno).status(Response.Status.OK).build();
+        }
+        return Response.ok(pokemon).status(Response.Status.NOT_FOUND).build();
+    }
+
+    @DELETE
+    @Path("{id:[1-9][0-9]*}")
+    @Transactional
+    public Response deletePokemon(@PathParam("id") Long id) {
+        Pokemon pokemon = Pokemon.findById(id);
+        if (pokemon != null && pokemon.isPersistent()) {
+            pokemon.delete();
+            return Response.ok(pokemon).status(Response.Status.NO_CONTENT).build();
+        }
+        return Response.ok(pokemon).status(Response.Status.NOT_FOUND).build();
     }
 }
